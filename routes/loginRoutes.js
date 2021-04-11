15043619456c2070b4240e2dfbe9aec7f2aa74f7
AC_Marvel <gil_1_10@hotmail.com>
2021-04-11 17:26:25 +0100

    ...

---
+++ routes/loginRoutes.js
@@ -0,0 +1,30 @@
+var express = require('express');
+var router = express.Router();
+var logModel = require("../models/loginModel");
+
+/** Router de login */
+router.post('/login', function (req, res, next){
+  logModel.login(req.body, function (status, result) {
+    if (status.code == 200)
+    res.send(result);
+  else {
+    res.statusMessage = status.status;
+    res.status(status.code).send({});
+  }
+  });
+});
+
+/**Router de Registar*/
+router.post('/registar', function (req, res, next) {
+  logModel.registar(req.body, function (status, result) {
+    if (status.code == 200)
+    res.send(result);
+  else {
+    res.statusMessage = status.status;
+    res.status(status.code).send({});
+   }
+ });    
+});
+
+module.exports = router;
+
